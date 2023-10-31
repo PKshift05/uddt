44baed573fad1e11cfb56be275de6b3c1c2bd9c4
quang <quang05012003@gmail.com>
2023-10-31 21:10:43 +0700

    vietquang

--- App.js
+++ App.js
@@ -21,53 +21,53 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-  
-      <Provider store={store}>
 
-        <PaperProvider >
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-              {/* <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ animation: 'default' }} />            
+    <Provider store={store}>
+
+      <PaperProvider >
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ animation: 'default' }} />
             <Stack.Screen
-            name="RegisterScreen"
-            component={RegisterScreen}
-          options={{ animation: 'slide_from_right' }} /> */}
-              <Stack.Screen name="Tab" component={TabNavigator} options={{ animation: 'default' }} />
-              <Stack.Screen
-                name="ListTicket"
-                component={ListTicket}
-                options={{ animation: 'default' }} />
+              name="RegisterScreen"
+              component={RegisterScreen}
+              options={{ animation: 'slide_from_right' }} />
+            <Stack.Screen name="Tab" component={TabNavigator} options={{ animation: 'default' }} />
+            <Stack.Screen
+              name="ListTicket"
+              component={ListTicket}
+              options={{ animation: 'default' }} />
 
-              <Stack.Screen
-                name="MovieDetails"
-                component={MovieDetailScreen}
-                options={{ animation: 'slide_from_right' }} />
-              <Stack.Screen
-                name="SeatBooking"
-                component={SeatBookingScreen}
-                options={{ animation: 'slide_from_bottom' }} />
-              <Stack.Screen
-                name="SettingAccount"
-                component={SettingAccount}
-                options={{ animation: 'default' }} />
-              <Stack.Screen
-                name="SettingApp"
-                component={SettingApp}
-                options={{ animation: 'default' }} />
-              <Stack.Screen
-                name="SettingOffer"
-                component={SettingOffer}
-                options={{ animation: 'default' }} />
-              <Stack.Screen
-                name="SettingAbout"
-                component={SettingAbout}
-                options={{ animation: 'default' }} />
+            <Stack.Screen
+              name="MovieDetails"
+              component={MovieDetailScreen}
+              options={{ animation: 'slide_from_right' }} />
+            <Stack.Screen
+              name="SeatBooking"
+              component={SeatBookingScreen}
+              options={{ animation: 'slide_from_bottom' }} />
+            <Stack.Screen
+              name="SettingAccount"
+              component={SettingAccount}
+              options={{ animation: 'default' }} />
+            <Stack.Screen
+              name="SettingApp"
+              component={SettingApp}
+              options={{ animation: 'default' }} />
+            <Stack.Screen
+              name="SettingOffer"
+              component={SettingOffer}
+              options={{ animation: 'default' }} />
+            <Stack.Screen
+              name="SettingAbout"
+              component={SettingAbout}
+              options={{ animation: 'default' }} />
 
-            </Stack.Navigator>
-          </NavigationContainer>
-        </PaperProvider>
-      </Provider>
-    
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
+
   );
 }
 
